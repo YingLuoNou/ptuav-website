9ddf9eadd8d9ff0dedeb819664f03274f552a0e3
YingLuoNou <YingLuoNou@users.noreply.github.com>
2026-01-28 16:51:33 +0800

    fix some svg

--- src/config/contacts.ts
+++ src/config/contacts.ts
@@ -3,7 +3,7 @@ export const CONTACT_CONFIG = [
     platform: "公司地址",
     address: "浙江省宁波市海曙区文化路88号D座3楼311室",
     // 这里使用简单的 SVG 路径字符串，或者你可以留空
-    svg: ``
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-location"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" /></svg>`
   },
   {
     platform: "Email",
@@ -13,6 +13,6 @@ export const CONTACT_CONFIG = [
   {
     platform: "客服电话",
     address: "18835020985",
-    svg: `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>`
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-phone"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>`
   }
 ];
